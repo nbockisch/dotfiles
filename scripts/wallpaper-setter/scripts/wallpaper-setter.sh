5c3379badddd0ef2f7b28f34be02cfb59a1709eb
Nathan Bockisch <nbockisch@protonmail.com>
2026-01-15 23:15:12 -0600

    Added the wallpaper setter script and wallust/wofi configs

--- scripts/wallpaper-setter/scripts/wallpaper-setter.sh
+++ scripts/wallpaper-setter/scripts/wallpaper-setter.sh
@@ -1,1 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+WALLPAPER_DIR="$HOME/pictures/wallpapers/"
+
+menu() {
+    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+}
+
+main() {
+    choice=$(menu | wofi --show dmenu --prompt "Select Wallpaper:" -n)
+    selected_wallpaper=$(echo "$choice" | sed 's/^img://')
+    wallust run $selected_wallpaper
+    swaybg -m fill -i $selected_wallpaper
+}
+
+main
