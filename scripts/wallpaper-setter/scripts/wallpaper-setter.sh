1cc09ca09d1dc908e8bed9a5d9739657a9cefedc
Nathan Bockisch <nbockisch@protonmail.com>
2026-02-07 17:50:57 -0600

    Wofi styling, show wallpaper filenames in the wallpaper picker

--- scripts/wallpaper-setter/scripts/wallpaper-setter.sh
+++ scripts/wallpaper-setter/scripts/wallpaper-setter.sh
@@ -3,15 +3,20 @@
 WALLPAPER_DIR="$HOME/pictures/wallpapers/"
 
 menu() {
-    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+    # The awk formatting allows both the image and the image name to be displayed in Wofi
+    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{ cmd = "basename \"" $0 "\""; cmd | getline output; close(cmd); print "img:"$0":text:"output}'
 }
 
 main() {
     choice=$(menu | wofi --style /home/nathan/.config/wofi/style.css --show dmenu --prompt "Select Wallpaper:" -n)
-    selected_wallpaper=$(echo "$choice" | sed 's/^img://')
-    cp $selected_wallpaper $WALLPAPER_DIR/current
-    wallust run $WALLPAPER_DIR/current
-    swaybg -m fill -i $WALLPAPER_DIR/current
+    selected_wallpaper=$(echo "$choice" | awk -F ':' '{ print $2 }')
+
+    # Only set the color scheme and cache the wallpaper if one was selected
+    if [[ -n $selected_wallpaper ]]; then
+        cp $selected_wallpaper $WALLPAPER_DIR/current
+        wallust run $WALLPAPER_DIR/current
+        swaybg -m fill -i $WALLPAPER_DIR/current
+    fi
 }
 
 main
